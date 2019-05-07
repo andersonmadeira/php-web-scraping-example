5a8762be4d573a36442fd23db58d1b687716dc38
Anderson Madeira <andersonmadeiracs@gmail.com>
2019-05-06 22:33:25 -0300

    Fetching data from rottent tomatoes
    Opening this week

--- fetch.php
+++ fetch.php
@@ -1,5 +1,25 @@
 <?php
 
+use Goutte\Client;
+use GuzzleHttp\Client as GuzzleClient;
+use Symfony\Component\DomCrawler\Crawler;
+
 require __DIR__ . '/vendor/autoload.php';
 
-// scrap here
+const WEEK_MOVIE_LIST_SELECTOR = '#Opening td.middle_col';
+const ROTTEN_TOMATOES_LINK = 'https://www.rottentomatoes.com/';
+
+$client = new Client();
+$guzzleClient = new GuzzleClient([
+    'timeout' => 60
+]);
+
+$client->setClient($guzzleClient);
+
+$crawler = $client->request('GET', ROTTEN_TOMATOES_LINK);
+
+print "Opening this week:\n\n";
+
+$crawler->filter(WEEK_MOVIE_LIST_SELECTOR)->each(function (Crawler $node) {
+     print trim($node->text()) . "\n";
+});
